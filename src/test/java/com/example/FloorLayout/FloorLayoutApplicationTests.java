8c516c8ab73767ec6f1c211c08c5203522d0879d
NazarZhyvchyn <44567357+NazarZhyvchyn@users.noreply.github.com>
2021-02-13 16:26:48 +0200

    Create FloorLayoutApplicationTests.java

---
+++ src/test/java/com/example/FloorLayout/FloorLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FloorLayout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FloorLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
